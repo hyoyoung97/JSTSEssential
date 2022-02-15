d83528201826aed52c2fd622cb6562c730c52f71
Ayoung Kim <hyoyoung97@naver.com>
2022-02-15 16:23:30 +0900

    CH03_03 - 복습

--- CH03/app.js
+++ CH03/app.js
@@ -1,28 +1,28 @@
+//`사용 => `${}`
+
 const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-//false -> 비동기를 쓰지 않음
-ajax.open('GET', NEWS_URL, false);
+//ajax로 화면에 뿌릴 데이터 받기
+ajax.open('GET', NEWS_URL, false);//false -> 비동기를 쓰지 않음()
 ajax.send();
-
 const newsFeed = JSON.parse(ajax.response);
+
 const ul = document.createElement('ul');
 
-window.addEventListener('hashchange',function () {
+//hashchange가 일어났을 때 (click 대신쓰임)
+window.addEventListener('hashchange', () => {
   const id = location.hash.substr(1);
 
   ajax.open('GET', CONTENT_URL.replace('@id', id), false);
   ajax.send();
-
   const newsContent = JSON.parse(ajax.response);
-  const tite = document.createElement('h1');
+  const title = document.createElement('h1');
 
   content.innerHTML = newsContent.title;
-  content.appendChild(tite);
-
-
+  content.appendChild(title);
 });
 
 for(let i = 0; i < 10; i++) {
@@ -30,7 +30,6 @@ for(let i = 0; i < 10; i++) {
   const a = document.createElement('a');
 
   a.href = '#';
-  //`${}`
   a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
 
   li.appendChild(a);
